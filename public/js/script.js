48e5d928267daf52663b4955c95a67374188571f
Jose Olinda <joseolindad@yahoo.com.br>
2023-06-18 21:53:42 -0300

    Refatoração para melhorar legilidade

--- public/js/script.js
+++ public/js/script.js
@@ -17,128 +17,130 @@ const videos = [
     cover: "/public/tears-of-steel-cover.jpg",
     poster: "/public/posters/tears-of-steel.jpg"
   }
-]
+];
 
-const videoPlayer = document.getElementById("videoPlayer")
-const filmChoice = document.forms["filme-choice"]
-const filmName = document.getElementById("film-name")
-const filmSelect = document.getElementById("filme-select")
-const filmPosters = document.getElementById("film-posters").getElementsByTagName("p")
-const filmPostersImages = document.getElementById("film-posters").getElementsByTagName("img")
+const videoPlayer = document.getElementById("videoPlayer");
+const filmChoice = document.forms["film-choice"];
+const filmName = document.getElementById("film-name");
+const filmSelect = document.getElementById("film-select");
+const filmPosters = document.querySelectorAll("#film-posters p");
+const filmPostersImages = document.querySelectorAll("#film-posters img");
 
-filmChoice.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const formData = new FormData(event.target)
-  const filme = formData.get("filme")
-  filmName.innerHTML = videos[filme - 1].name
-  videoPlayer.src = `/video?filme=${filme}`
-  videoPlayer.poster = videos[filme - 1].cover
-  videoPlayer.load()
-  // videoPlayer.controls = false
-  videoPlayer.play()
-  updateStats({
-    time_play: moment().format("HH:mm:ss")
-  })
-})
+filmChoice.addEventListener("submit", handleFilmChoiceSubmit);
+filmSelect.addEventListener("change", handleFilmSelectChange);
+filmPosters.forEach(poster => {
+  poster.addEventListener("click", handlePosterClick);
+});
 
-filmSelect.addEventListener("change", (event) => {
-  const selectedFilm = filmSelect.value
-  for (let i = 0; i < filmPostersImages.length; i++) {
-    filmPostersImages[i].parentElement.classList.remove("selected")
-  }
-  filmPostersImages[selectedFilm - 1].parentElement.classList.add("selected")
-})
+function handleFilmChoiceSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const film = formData.get("film-select") || 1;
+  const selectedVideo = videos[film - 1];
+  filmName.textContent = selectedVideo.name;
+  videoPlayer.src = `/video?filme=${film}`;
+  videoPlayer.poster = selectedVideo.cover;
+  videoPlayer.load();
+  videoPlayer.play();
+  updateStats({ time_play: moment().format("HH:mm:ss") });
+}
 
-for (let poster of filmPosters) {
-  poster.addEventListener("click", (event) => {
-    const selectedFilm = poster.dataset.film
-    filmSelect.value = selectedFilm
-    filmSelect.dispatchEvent(new Event("change"))
-    filmChoice.dispatchEvent(new Event("submit"))
-  })
+function handleFilmSelectChange(event) {
+  const selectedFilm = filmSelect.options[filmSelect.selectedIndex].value;
+  filmPostersImages.forEach(image => {
+    image.parentElement.classList.remove("selected");
+  });
+  console.log(filmSelect);
+  filmPostersImages[selectedFilm - 1].parentElement.classList.add("selected");
+}
+
+function handlePosterClick(event) {
+  const selectedFilm = event.target.dataset.film;
+  filmSelect.value = selectedFilm;
+  filmSelect.dispatchEvent(new Event("change"));
+  filmChoice.dispatchEvent(new Event("submit"));
 }
 
 // Mostrar estatísticas do vídeo
-const videoName = document.querySelector("#video-name")
-const resolution = document.querySelector("#resolution")
-const bitrate = document.querySelector("#bitrate")
-const fileSize = document.querySelector("#file-size")
-const duration = document.querySelector("#duration")
+const videoName = document.querySelector("#video-name");
+const resolution = document.querySelector("#resolution");
+const bitrate = document.querySelector("#bitrate");
+const fileSize = document.querySelector("#file-size");
+const duration = document.querySelector("#duration");
 
-const protocol = document.querySelector("#protocol")
-const latency = document.querySelector("#latency")
-const packetsInput = document.querySelector("#p-input")
-const packetsOutput = document.querySelector("#p-output")
-const bytesInputSize = document.querySelector("#b-input-size")
-const playTime = document.querySelector("#play-time")
-const firstByte = document.querySelector("#first-byte")
-const lastByte = document.querySelector("#last-byte")
-const downloadTime = document.querySelector("#download-time")
-const bufferingPercent = document.querySelector("#buffering-percent")
-const bufferingTime = document.querySelector("#buffering-time")
-let pausesCount = 0
-const pauses = document.querySelector("#pauses")
-const videoPlayTimeTotal = document.querySelector("#play-time-total")
-const videoPlayTimePercent = document.querySelector("#play-time-percent")
+const protocol = document.querySelector("#protocol");
+const latency = document.querySelector("#latency");
+const packetsInput = document.querySelector("#p-input");
+const packetsOutput = document.querySelector("#p-output");
+const bytesInputSize = document.querySelector("#b-input-size");
+const playTime = document.querySelector("#play-time");
+const firstByte = document.querySelector("#first-byte");
+const lastByte = document.querySelector("#last-byte");
+const downloadTime = document.querySelector("#download-time");
+const bufferingPercent = document.querySelector("#buffering-percent");
+const bufferingTime = document.querySelector("#buffering-time");
+const pauses = document.querySelector("#pauses");
+const videoPlayTimeTotal = document.querySelector("#play-time-total");
+const videoPlayTimePercent = document.querySelector("#play-time-percent");
 
-updateStats = (videoinf) => {
-  videoName.innerText = filmName.innerText || "---"
+let pausesCount = 0;
+function updateStats(videoInfo) {
+  videoName.textContent = filmName.textContent || "---";
   videoPlayer.addEventListener("loadedmetadata", function () {
-    const v = {
-      width: videoPlayer.videoWidth,
-      height: videoPlayer.videoHeight,
-      duration: moment.duration(videoPlayer.duration, "seconds"),
-      file_size: videoPlayer.size,
-      bitrate: videoPlayer.size / videoPlayer.duration,
-    }
+    const videoWidth = videoPlayer.videoWidth;
+    const videoHeight = videoPlayer.videoHeight;
+    const videoDuration = moment.duration(videoPlayer.duration, "seconds");
+    const videoFileSize = videoPlayer.size;
+    const videoBitrate = videoFileSize / videoPlayer.duration;
 
-    resolution.innerText = (v.width + "x" + v.height) || "---"
-    bitrate.innerText = (Math.round(v.bitrate / 1000) + " kbps") || "---"
-    duration.innerText = `${v.duration.minutes()}min ${v.duration.seconds()}s`
+    resolution.textContent = `${videoWidth}x${videoHeight}` || "---";
+    bitrate.textContent = `${Math.round(videoBitrate / 1000)} kbps` || "---";
+    duration.textContent = `${videoDuration.minutes()}min ${videoDuration.seconds()}s`;
 
-    playTime.innerText = videoinf.time_play
-    firstByte.innerText = moment().format("HH:mm:ss")
-  })
-  
-  let downloadStartTime = null
-  let downloadEndTime = null
-  let finishDownloadTime = null
-  videoPlayer.addEventListener("loadstart", function () {
-    pausesCount = 0
-    pauses.innerText = pausesCount
-    downloadStartTime = new Date()
-  })
-
-  videoPlayer.addEventListener("waiting", function () {
-    pausesCount++
-    pauses.innerText = pausesCount
+    playTime.textContent = videoInfo.time_play;
+    firstByte.textContent = moment().format("HH:mm:ss");
   });
 
-  // Evento atualização de tempo
-  videoPlayer.addEventListener('progress', function () {
+  let downloadStartTime = null;
+  let downloadEndTime = null;
+  let finishDownloadTime = null;
+  videoPlayer.addEventListener("loadstart", function () {
+    pausesCount = 0;
+    pauses.textContent = pausesCount;
+    downloadStartTime = new Date();
+  });
+
+  videoPlayer.addEventListener("waiting", function () {
+    pausesCount++;
+    pauses.textContent = pausesCount;
+  });
+
+  videoPlayer.addEventListener("progress", function () {
     if (downloadStartTime) {
-      let currentTime = new Date()
-      downloadEndTime = moment.duration(currentTime - downloadStartTime)
-      downloadTime.innerText = `${downloadEndTime.minutes()}min ${downloadEndTime.seconds()}s`
+      const currentTime = new Date();
+      downloadEndTime = moment.duration(currentTime - downloadStartTime);
+      downloadTime.textContent = `${downloadEndTime.minutes()}min ${downloadEndTime.seconds()}s`;
     }
-    let percent = 0
+    let percent = 0;
     if (videoPlayer.buffered.length > 0) {
-      percent = Math.round((videoPlayer.buffered.end(0) / videoPlayer.duration) * 100)
-      bufferingPercent.innerText = `${percent}%`
+      percent = Math.round((videoPlayer.buffered.end(0) / videoPlayer.duration) * 100);
+      bufferingPercent.textContent = `${percent}%`;
     }
     if (percent === 100) {
-      finishDownloadTime = moment().format("HH:mm:ss")
-      bufferingTime.innerText = `${finishDownloadTime}s`
+      finishDownloadTime = moment().format("HH:mm:ss");
+      bufferingTime.textContent = `${finishDownloadTime}s`;
     }
   });
 
   videoPlayer.addEventListener("timeupdate", function () {
-    let timePercent = Math.round((videoPlayer.currentTime / videoPlayer.duration) * 100)
-    videoPlayTimePercent.innerText = `${timePercent}%`
+    const timePercent = Math.round((videoPlayer.currentTime / videoPlayer.duration) * 100);
+    videoPlayTimePercent.textContent = `${timePercent}%`;
     if (timePercent === 100) {
-      videoPlayTimeTotal.innerText = `${moment().format("HH:mm:ss")}s`
+      videoPlayTimeTotal.textContent = `${moment().format("HH:mm:ss")}s`;
     }
   });
+  
+  videoPlayer.controls = false;
 }
 
-updateStats({ play_time: 0 })
+updateStats({ play_time: 0 });
