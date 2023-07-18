14f7f42d09c24535e21ffb5895fad74c42b8fc99
Jose Olinda <joseolindad@yahoo.com.br>
2023-07-18 11:55:09 -0300

    Fix limit json

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const { Console } = require("console")
 const e = require("express")
 
 app.use("/public", express.static("public"))
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
